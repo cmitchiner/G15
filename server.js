f2ef144853bc08c2beafc5c154b2d4b26c7e6c88
cmitchiner <c.l.mitchiner@student.tue.nl>
2021-05-10 12:14:27 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+//Include express and path libraries
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const app = express();
+
+//Formating the uploaded file
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '/uploads/'));
+    },
+    filename: (req, file, cb) => {
+        const { originalname } = file;
+        cb(null, 'data.csv');
+    }
+});
+const upload = multer({ storage: storage });
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+//Tell the webpage the root HTML file
+
+//Add the upload function to the website
+app.post('/upload', upload.single("Csv data file"), (req, res) => {
+    return res.json({ status : "File uploaded successfully!" });
+});
+
+//Send website to local host port 5050 and print server running to console
+app.listen(process.env.PORT || 5050, () => console.log("Server running..."));

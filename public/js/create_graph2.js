ec84cacc49d844624f1149192c5b110a99d74dff
0MrTime0 <henrik.ispanki@gmail.com>
2021-06-04 16:53:54 +0200

    idk man, pain

--- public/js/create_graph2.js
+++ public/js/create_graph2.js
@@ -1,17 +1,49 @@
 var g1;
-  d3.json("/data/enron_dataset.json", function (g) {
-    g1 = g;
-    changeSlider(g1, 0, 1);
-  });
+    d3.json("/data/GraphInput.json", function (g) {
+        
+        //Filter data by date from sliders
+        var month = 12;
+        var year = 2000;
+        g1 = {"nodes" : [], "links" : []};
+
+        var filteredLinks = g.links.filter(d => parseInt(d.value.substr(0, 4)) <= year && parseInt(d.value.substr(5, 7)) <= month);
+        filteredLinks = filteredLinks.map(u => ({ source: u.source, target: u.target}));
+        console.log("filteredLinks", filteredLinks);
+        const groupArray = (filteredLinks = []) => {
+            // create map
+            let map = new Map()
+            for (let i = 0; i < filteredLinks.length; i++) {
+                const s = JSON.stringify(filteredLinks[i]);
+                if (!map.has(s)) {
+                    map.set(s, {
+                        source: filteredLinks[i].source,
+                        target: filteredLinks[i].target,
+                        value: 1,
+                    });
+                } else {
+                    map.get(s).value++;
+                }
+            }
+            const res = Array.from(map.values())
+            return res;
+        };
+        
+        console.log("filteredLinks", groupArray(filteredLinks));
+
+        g1.nodes = g.nodes;
+        g1.links = groupArray(filteredLinks);        
+        console.log("g1", g1);
+        changeSlider(g1, 0, 1);
+    });
 
 
-  var slider = document.getElementById("myRange");
-  var output = document.getElementById("demo");;
-  output.innerHTML = slider.value;
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");;
+    output.innerHTML = slider.value;
 
 
 
-  slider.oninput = function () {
-    output.innerHTML = this.value;
-    changeSlider(g1, 0, this.value);
-  }
+    slider.oninput = function () {
+        output.innerHTML = this.value;
+        changeSlider(g1, 0, this.value);
+    }
